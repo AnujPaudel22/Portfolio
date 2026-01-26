efbed0d558e2b0440f1c282467d59433524dd148
AnujPaudel22 <anuj.paudel2015@gmail.com>
2026-01-26 21:20:57 +0545

    First Commit

---
+++ src/assets/conponents/Navbar.tsx
@@ -0,0 +1,17 @@
+
+
+const Navbar = () => {
+  return (
+    <div>
+    <div className="flex gap-12 justify-end bg-gray-700 pr-15 text-white sticky">
+      <div>Home</div>
+      <div>About</div>
+      <div>Skills</div>
+      <div>Projects</div>
+      <div>Contacts</div>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
